6f997b901ba5d7230ac5b499ea7e44ca0931f5e6
bustinjailey <bustinjailey@gmail.com>
2013-09-26 14:27:13 -0400

    Gets closest code from lat/lon now

--- web.js
+++ web.js
@@ -1,42 +1,87 @@
-var express = require('express');
-var http = require('http');
-var jsonQuery = require('json-query');
-
+var express = require("express");
+var http = require("http");
+var __ = require("underscore");
+var fs = require("fs");
+var csv = require("csv");
+var $ = require("jQuery");
+var jsonQuery = require("json-query");
 var app = express();
 app.use(express.logger());
-var json = '';
 
-var latlng = '39.0780,-77.5575'
+var lat = "39.0780";
+var lon = "-77.5575";
 
-var options = {
-	host: 'maps.googleapis.com',
-	port: 80,
-	path: '/maps/api/geocode/json?latlng=' + latlng + '&sensor=false'
-};
+var filePath = "./data/GlobalAirportDatabase.txt";
+var transformedData = "";
 
-http.get(options, function(res) {
-
-	var body = '';
-
-	res.on('data', function(data){
-		body += data;
-	});
-
-	res.on('end', function(){
-		json = JSON.parse(body);
-		console.log('json done');
-	});
-
-}).on('error', function(e) {
-	console.log("Got error: " + e.message);
+// Dump CSV in to JSON
+csv().from(filePath, {delimiter: ":"}).to.array(function(data){
+	transformedData = transformData(data);
+	console.log("successful parse and transform");
 });
 
-app.get('/', function(req, res){
-	var result = jsonQuery('results[types=airport].name', json)
-	res.send(json);
+// Only works for USA right now, limited by data
+function transformData(origData){
+	var newData = [];
+	for (var i = 0; i < origData.length; i++){
+		var o = origData[i];
+		if(o[4] == "USA"){ // Get USA values only
+			var latSign = 1;
+			var lonSign = 1;
+			
+			if(o[8] == "S"){
+				latSign = -1;
+			}		
+			if(o[12] == "W" || o[12] == "U"){ // U: unknown, but in USA will always be western hemisphere.
+				lonSign = -1;
+			}
+			
+			newData[i] = 
+			{
+					"icao" : o[0],
+					"iata" : o[1],
+					"name" : o[2],
+					"city" : o[3],
+					"country" : o[4],
+					"lat" : (parseFloat(o[5]) + parseFloat(o[6])/60 + parseFloat(o[7])/3600) * latSign,
+					"lon" : (parseFloat(o[9]) + parseFloat(o[10])/60 + parseFloat(o[11])/3600) * lonSign,
+					"alt" : o[13]
+			};	
+		}
+	}
+	
+	return newData.filter(function(n){return n}); // remove nulls from skipping other countries
+}
+
+function distance(x1, y1, x2, y2){
+	return Math.sqrt(Math.pow(x2-x1, 2) + Math.pow(y2-y1, 2));
+}
+ 
+function getClosestAirport(lat, lon, json) {
+	var minDist = null;
+	var bestMatch = [];
+	
+	$.each(json, function(){
+		var thisDist = distance(lat, lon, this.lat, this.lon);
+		
+		if(minDist == null || minDist > thisDist){
+			minDist = thisDist;
+			bestMatch = this;
+		}
+	});
+	
+	return bestMatch;
+}
+
+app.get("/", function(req, res){
+	// On web request, send data file stuff	
+	
+	var closestAirport = getClosestAirport(lat, lon, transformedData);
+		
+	res.send(closestCode);
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-	console.log('Listening on ' + port);
+	console.log("Listening on " + port);
 });
