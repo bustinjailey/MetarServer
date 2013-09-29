b4d66f691288c918038143d0afd1c5053e75d79e
Bailey, Justin <jbailey@cvent.com>
2013-09-29 19:03:44 -0400

    dkld

--- server.js
+++ server.js
@@ -100,7 +100,7 @@ app.post("/", function(req, res){
 			// Just send the second line
 			var secondLine = metar.split("\n").splice(1)[0];
 			
-			var out = JSON.stringify({1: secondLine.substring(0,4)});
+			var out = JSON.stringify({1: secondLine.substring(0, secondLine.indexOf("RMK"))});
 			console.log(out);
 			res.send(out);
 		})
